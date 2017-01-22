1582e3e1275ccddb40564b3745eac39dc3340bad
BENMANSER Noureddine <um5ben@gmail.com>
2017-01-22 16:04:28 +0000

    Basic Algorithms in JS
    Basic Algorithms in JS

---
+++ Chunky Monkey.js
@@ -0,0 +1,13 @@
+function chunkArrayInGroups(arr, size) {
+  // Break it up
+  // It's already broken :(
+  arr = arr.slice();
+  var arr2 = [];
+  for(var i = 0, len = arr.length; i < len; i+=size) {
+    arr2.push(arr.slice(0, size));
+    arr = arr.slice(size);
+  }
+  return arr2;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
